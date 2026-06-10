1b81fe42fe3d79686fdee9959551d9be6955418a
An Quach <anquach.dev@gmail.com>
2026-06-10 20:26:37 +0700

    Initial Commit

---
+++ S2STranslate/S2STranslateApp.swift
@@ -0,0 +1,17 @@
+//
+//  S2STranslateApp.swift
+//  S2STranslate
+//
+//  Created by An Quach on 10/6/26.
+//
+
+import SwiftUI
+
+@main
+struct S2STranslateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
